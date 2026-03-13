6d108b0d2744e4406b68617e028c96c7a4292fd4
Postert <51877726+Postert@users.noreply.github.com>
2026-03-13 15:57:56 +0100

    Add test functionality

--- Sources/SFCGAL_SPM/SFCGAL.swift
+++ Sources/SFCGAL_SPM/SFCGAL.swift
@@ -1,2 +1,8 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
+
+public func helloWorld() -> String {
+    let message = "Hello, World from SFCGAL!"
+    print(message)
+    return message
+}
